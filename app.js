5c9bb06de45946a7bae02fee2922fc75398b024f
huevangxp <huevang933@gmail.com>
2025-02-12 19:20:34 +0700

    new

---
+++ app.js
@@ -0,0 +1,21 @@
+/* Created by Tivotal */
+
+const wrapper = document.querySelector(".wrapper");
+
+wrapper.addEventListener("click", () => {
+  wrapper.classList.toggle("open");
+});
+
+function createHearts() {
+  const heartsContainer = document.querySelector(".hearts");
+  for (let i = 0; i < 15; i++) {
+      let heart = document.createElement("div");
+      heart.classList.add("heart");
+      heart.style.left = Math.random() * 100 + "vw";
+      heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+      heartsContainer.appendChild(heart);
+      setTimeout(() => { heart.remove(); }, 5000);
+  }
+}
+
+setInterval(createHearts, 1000);
